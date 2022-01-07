73ad56e9c103a11b97a6820650909ca380119977
neolei <neolei@tencent.com>
2022-01-07 19:03:38 +0800

    1.2.0 debug job 2.8

--- src/backend/plugins/common/default/__init__.py
+++ src/backend/plugins/common/default/__init__.py
@@ -13,12 +13,8 @@ either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
 
-import copy
-
 from opsbot import on_command, CommandSession, on_natural_language
-from opsbot.exceptions import HttpFailed
 from opsbot.log import logger
-from component import RedisClient
 from .api import (
     render_welcome_msg, render_biz_msg, render_default_intent, is_cache_visit,
     bind_group_biz, Flow
@@ -40,7 +36,7 @@ async def _(session: CommandSession):
     if 'event_key' in session.ctx:
         return
 
-    msg = Flow(session).render_welcome_msg()
+    msg = await Flow(session).render_welcome_msg()
     await session.send('', msgtype='template_card', template_card=msg)
 
 
@@ -61,30 +57,6 @@ async def _(session: CommandSession):
         logger.info('bind biz error')
         return
 
-    msg = flow.render_welcome_msg()
+    msg = await flow.render_welcome_msg()
     await session.send('', msgtype='template_card', template_card=msg)
 
-
-@on_command('opsbot_help', aliases=('opsbot_help', ))
-async def _(session: CommandSession):
-    _, biz_name, user_id = session.ctx['event_key'].split('|')
-    key = f'{session.bot.config.RTX_NAME}:{user_id}'
-    redis_client = RedisClient(env="prod")
-    group_id = redis_client.hash_get("chat_opsbot_help", key)
-    if not group_id:
-        user_ids = copy.copy(DEFAULT_HELPER)
-        user_ids.append(session.ctx['msg_sender_code'])
-        response = await session.bot.create_chat(f'{session.bot.config.RTX_NAME}：关于{user_id}的{biz_name}业务问题咨询',
-                                                 user_ids)
-        group_id = response['chatid']
-        redis_client.hash_set("chat_opsbot_help", key, group_id)
-
-    await session.send(f'{user_id} 已为您拉群处理，请关注群聊消息')
-    await session.send('这里', receiver={'type': 'group', 'id': group_id})
-
-
-@on_command('opsbot_tool_extra', aliases=('opsbot_default_intent', ))
-async def _(session: CommandSession):
-    _, biz_id, user_id = session.ctx['event_key'].split('|')
-    msg = await render_default_intent(session, lambda x: x['key'] in DEFAULT_INTENT_CATEGORY)
-    await session.send('', msgtype='rich_text', rich_text=msg)
