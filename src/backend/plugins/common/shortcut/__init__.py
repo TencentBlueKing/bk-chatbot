c70f6a815278ea00207d877d14261f6385f49b3b
neolei <neolei@tencent.com>
2022-02-11 20:10:23 +0800

    1.4.0 任务推荐 功能测试 0.3

--- src/backend/plugins/common/shortcut/__init__.py
+++ src/backend/plugins/common/shortcut/__init__.py
@@ -33,7 +33,7 @@ async def create_bk_shortcut(session: CommandSession):
     content = f'''>**{platform} TIP**
             >请输入快捷键名称，**最少输入8个字符**, 每个人每个业务最多10个快捷键'''
     shortcut_name, _ = session.get('shortcut_name', prompt='...', msgtype='markdown', markdown={'content': content})
-    sc_handler = ShortcutHandler(shortcut_name, session)
+    sc_handler = ShortcutHandler(session, shortcut_name)
     while not sc_handler.validate_name():
         del session.state['shortcut_name']
         shortcut_name, _ = session.get('shortcut_name', prompt='...', msgtype='markdown', markdown={'content': content})
@@ -47,15 +47,29 @@ async def create_bk_shortcut(session: CommandSession):
 @on_command('bk_shortcut_execute')
 async def execute_bk_shortcut(session: CommandSession):
     shortcut = session.state['info']
-    sc_handler = ShortcutHandler(shortcut.name, session)
+    sc_handler = ShortcutHandler(session)
     msg = await sc_handler.execute_task(shortcut)
     await session.send('', msgtype='template_card', template_card=msg)
 
 
+@on_command('bk_shortcut_list')
+async def list_bk_shortcut(session: CommandSession):
+    msg = ShortcutHandler(session).render_shortcut_list()
+    await session.send('', msgtype='template_card', template_card=msg)
+
+
+@on_command('bk_shortcut_delete')
+async def delete_bk_shortcut(session: CommandSession):
+    sc_handler = ShortcutHandler(session)
+    msg = sc_handler.delete()
+    content = f'''>**快捷键 TIP**
+                    >快捷键「{msg}」删除成功'''
+    await session.send('', msgtype='markdown', markdown={'content': content})
+
 @on_natural_language
 async def _(session: NLPSession):
     msg = session.msg_text.strip()
-    sc_handler = ShortcutHandler(msg, session)
-    shortcut = sc_handler.find()
+    sc_handler = ShortcutHandler(session, msg)
+    shortcut = sc_handler.find_one()
     if shortcut:
         return IntentCommand(100, 'bk_shortcut_execute', args={'info': shortcut})
--- src/backend/plugins/common/shortcut/api.py
+++ src/backend/plugins/common/shortcut/api.py
@@ -13,6 +13,7 @@ either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
 
+import time
 from typing import Dict, Union
 
 from opsbot import CommandSession
@@ -27,7 +28,7 @@ class ShortcutHandler(GenericTask):
     deal with alias or shortcut call
     save setting and directly run platform plugin
     """
-    def __init__(self, name: str, session: CommandSession, bk_biz_id: Union[str, int] = None):
+    def __init__(self, session: CommandSession, name: str = None, bk_biz_id: Union[str, int] = None):
         super().__init__(session, bk_biz_id, RedisClient(env='prod'))
         self.name = name
         self.orm_client = OrmClient()
@@ -44,11 +45,49 @@ class ShortcutHandler(GenericTask):
                                        bk_username=self.user_id, params=info)
         self.orm_client.add(shortcut_task)
 
-    def find(self):
+    def find_one(self):
         return self.orm_client.query(BKShortcutTask, 'first', bk_biz_id=self.biz_id,
                                      bk_username=self.user_id, name=self.name)
+
+    def find_all(self):
+        shortcuts = self.orm_client.query(BKShortcutTask, 'all', bk_biz_id=self.biz_id, bk_username=self.user_id)
+        return [{'id': str(item.id), 'text': item.name, 'is_checked': False} for item in shortcuts[:20]]
 
     async def execute_task(self, shortcut: BKShortcutTask):
         flow = SHORTCUT_PROTO[shortcut.bk_platform](self._session)
         result = flow.execute_task(shortcut.params)
-        return flow.render_job_plan_execute_msg(result, shortcut.params)
+        return getattr(flow, f'render_{shortcut.bk_platform.lower()}_plan_execute_msg')(result, shortcut.params)
+
+    def render_shortcut_list(self):
+        shortcuts = self.find_all()
+        template_card = {
+            'card_type': 'vote_interaction',
+            'source': {
+                'desc': '快捷键',
+                'desc_color': 1
+            },
+            'main_title': {
+                'title': '欢迎使用快捷键服务',
+                'desc': '选择删除'
+            },
+            'task_id': str(int(time.time() * 100000)),
+            'checkbox': {
+                'question_key': 'bk_shortcut_id',
+                'option_list': shortcuts
+            },
+            'submit_button': {
+                'text': '删除',
+                'key': 'bk_shortcut_delete'
+            }
+        }
+        return template_card
+
+    def delete(self):
+        try:
+            shortcut_id = self._session.ctx['SelectedItems']['SelectedItem']['OptionIds']['OptionId']
+        except KeyError:
+            return None
+
+        shortcut = self.orm_client.query(BKShortcutTask, 'first', id=int(shortcut_id))
+        self.orm_client.delete(shortcut)
+        return shortcut.name
