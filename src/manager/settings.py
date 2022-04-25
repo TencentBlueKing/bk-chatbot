76e0744eedb218061f005b4661893e8de329bc84
hchicken <342321394@qq.com>
2022-04-25 18:04:25 +0800

    fix:删除多余的依赖

--- src/manager/settings.py
+++ src/manager/settings.py
@@ -16,8 +16,6 @@ specific language governing permissions and limitations under the License.
 import os
 
 from blueapps.conf.default_settings import *  # noqa
-from blueapps.conf.log import get_logging_config_dict
-from django.conf.urls import include, url
 
 current_dir = os.path.dirname(__file__)
 
