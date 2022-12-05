4bc04b6923e67206b37a0dea475a203c5a1d39f3
neolei <neolei@tencent.com>
2022-12-05 19:30:26 +0800

    1.7.0 slack 1.3

--- src/backend/protocol/slack/message.py
+++ src/backend/protocol/slack/message.py
@@ -13,14 +13,81 @@ either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
 
+import re
+import json
+import time
+from typing import Iterable, Tuple, Union, List, Dict
 
-class Message:
-    pass
+from opsbot.adapter import (
+    Message as BaseMessage, MessageSegment as BaseMessageSegment,
+    MessageTemplate as BaseMessageTemplate
+)
+from opsbot.stdlib import escape, unescape
 
 
-class MessageSegment:
-    pass
+class MessageSegment(BaseMessageSegment):
+    def __delitem__(self, key):
+        pass
+
+    def __str__(self):
+        if self.is_text():
+            return escape(self.data.get('text', ''), escape_comma=False)
+
+        params = ','.join(('{}={}'.format(k, escape(str(v)))
+                           for k, v in self.data.items()))
+        if params:
+            params = ',' + params
+        return '[W:{type}{params}]'.format(type=self.type, params=params)
+
+    def is_text(self) -> bool:
+        return self.type == 'text'
+
+    @staticmethod
+    def text(text: str):
+        return MessageSegment(type_='text', data={'text': text})
 
 
-class MessageTemplate:
-    pass
+class Message(BaseMessage):
+    @staticmethod
+    def _normalized(msg_str: str) -> Iterable[MessageSegment]:
+        def iter_function_name_and_extra() -> Iterable[Tuple[str, str]]:
+            yield 'text', unescape(msg_str)
+
+        for function_name, extra in iter_function_name_and_extra():
+            if function_name == 'text':
+                if extra:
+                    # only yield non-empty text segment
+                    yield MessageSegment(type_=function_name,
+                                         data={'text': extra})
+            else:
+                data = {k: v for k, v in map(
+                    lambda x: x.split('=', maxsplit=1),
+                    filter(lambda x: x, (x.lstrip() for x in extra.split(',')))
+                )}
+                yield MessageSegment(type_=function_name, data=data)
+
+
+class MessageTemplate(BaseMessageTemplate):
+    @classmethod
+    def render_markdown_msg(cls):
+        pass
+
+    @classmethod
+    def render_welcome_msg(cls):
+        pass
+
+    @classmethod
+    def render_biz_list_msg(cls):
+        pass
+
+    @classmethod
+    def render_task_select_msg(cls):
+        pass
+
+    @classmethod
+    def render_task_execute_msg(cls):
+        pass
+
+    @classmethod
+    def render_task_filter_msg(cls):
+        pass
