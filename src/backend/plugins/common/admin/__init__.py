139e2535dd1b49973fcd145197369c9d81e91978
neolei <neolei@tencent.com>
2022-12-27 21:18:50 +0800

    1.8.0 i18n 1.9

--- src/backend/plugins/common/admin/__init__.py
+++ src/backend/plugins/common/admin/__init__.py
@@ -16,15 +16,19 @@ specific language governing permissions and limitations under the License.
 from opsbot import on_command, CommandSession
 
 from .api import Stat, on_admin
+from .settings import (
+    ADMIN_STAT_EXECUTION_KEY, ADMIN_STAT_EXECUTION_ALIAS,
+    ADMIN_STAT_EXECUTION_TITLE, ADMIN_STAT_EXECUTION_PREFIX
+)
 
 
-@on_command('bk_chat_stat_execution', aliases=('执行统计', ))
+@on_command(ADMIN_STAT_EXECUTION_KEY, aliases=ADMIN_STAT_EXECUTION_ALIAS)
 @on_admin
 async def _(session: CommandSession):
     stat = Stat()
     count = stat.stat_execution()
-    title = '<bold>执行统计<bold>'
-    content = f'<warning>您当前执行数「{count}」<warning>'
+    title = f'<bold>{ADMIN_STAT_EXECUTION_TITLE}<bold>'
+    content = f'<warning>{ADMIN_STAT_EXECUTION_PREFIX}「{count}」<warning>'
     msg_template = session.bot.send_template_msg('render_markdown_msg', title, content)
     await session.send(**msg_template)
     del stat
