7bbbbd95b594d90eabda01f01905f5e242bfa02e
hchicken <342321394@qq.com>
2023-08-15 11:15:45 +0800

    fix:必须超级管理员权限才能直接查看文档

--- urls.py
+++ urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.views import static
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -27,6 +28,7 @@ schema_view = get_schema_view(
         description="this is bkchat api",
     ),
     public=True,
+    permission_classes=(permissions.IsAdminUser,),
 )
 
 
